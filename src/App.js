711521386ed0d00cd43a01ba0d7410deceecea4f
Eric <eric7njuguna@gmail.com>
2020-04-06 18:15:07 +0300

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
+import {Cards, Chart, CountryPicker} from './components';
+import styles from './App.module.css';
+import { fetchData } from './api';
+
+
+
+
+
 class App extends React.Component{
+    async componentDidMount(){
+        const data = await fetchData();
+        console.log(data);
+    }
     render() {
         return(
-            <div>
-                <h1>
-                    App
-                </h1>
+            <div className = {styles.container}>
+                <Cards />
+                <CountryPicker />
+                <Chart />
             </div>
         )
     }
